b28793ccb37259f465b565a442c2d0db51a6ca2a
ywi19 <50904167+ywi19@users.noreply.github.com>
2019-05-22 10:01:29 +0200

    Update HelloWorld.java

--- GitHub/src/HelloWorld.java
+++ GitHub/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World 1");
+		System.out.println("Hello World 3");
 		System.out.println("Hello World 2");
 
 	}
